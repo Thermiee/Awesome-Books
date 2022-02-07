6d0e42246b1b99c4a39553066a0bb3650d3b82c5
Awais Amjed <awaisamjed171@gmail.com>
2022-02-07 15:53:44 +0500

    Add remove book Functionality

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
+class Book {
+  title;
+  author;
+
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
 const books = [];
 
 function addBook(title, author) {
-    books.push({
-        title: title,
-        author: author,
-    });
+  books.push(new Book(title, author));
+}
+
+function removeBook(book) {
+  for (let i=0; i<books.length; i++) {
+    if (books[i].title === book.title && books[i].author === book.author) {
+      books.splice(i, 1);
+      return;
+    }
+  }
 }
